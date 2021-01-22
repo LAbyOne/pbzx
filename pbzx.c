e3a647c044185b656f4b35a351e59b23584dec4c
LAbyOne <63520775+LAbyOne@users.noreply.github.com>
2021-01-22 12:18:15 +0100

    Update pbzx.c

--- pbzx.c
+++ pbzx.c
@@ -1,7 +1,7 @@
 /**
  * Copyright (C) 2017  Niklas Rosenstein
  * Copyright (C) 2014  PHPdev32
- *
+ * created 2014-06-20
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -15,8 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * \file pbzx.c
- * \created 2014-06-20
  */
 
 #include <errno.h>
